0814014eba6be668e87ce0ffa72be944c566a98f
Bekzat52 <bekzat@bekzat-IdeaPad-3-15ALC6>
2022-08-16 17:13:48 +0600

    user

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,20 @@ REST_FRAMEWORK = {
     )
 }
 
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+      'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+      }
+   }
+}
+
 from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),}
+
+AUTH_USER_MODEL = 'account.User'
